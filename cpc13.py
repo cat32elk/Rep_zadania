e9a447215e06c5dccf385e97c6c093cf2357cabd
cat32elk <ruslana.losenkova@gmail.com>
2026-08-02 20:33:39 +0300

    commit

---
+++ cpc13.py
@@ -0,0 +1,12 @@
+# # words = ["море", "солнце", "майка", "отпуск"]
+# # a_words = [i for i in words if i.startswith('м')]
+# # b_words = [i for i in words if i.endswith('е')]
+# # print(a_words)
+# # print(b_words)
+# #
+# # # s = [x for x in range(10) if x > 3]
+# # # print(s)
+# s1 = [10-x for x in range(11) if x < 6]
+# print(s1)
+
+a =['блистательно',]
